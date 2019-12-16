7359a456c162636e98f9b803bf0c3cd6356b486a
fihis <isifuo@gmail.com>
2019-12-16 22:15:31 +0300

    change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css';
 import React from 'react';
-import Output from './Output'
-import Userform from './Userform'
-import ErrorMsg from './ErrorMsg'
 import List from './List'
 import Home from './Home'
+import UserDetails from './UserDetails'
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,40 +11,8 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      user: {
-        name: '',
-        surname: '',
-        gender: ''
-      },
-      errorFlag: false
-    }
-  };
-
-  handleChange = (target, value) => {
-    this.setState({ user: { ...this.state.user, [target]: value }, errorFlag: false })
-  }
-
-  handleSubmit = () => {
-    for (let key in this.state.user) {
-      if (this.state.user[key] === '') {
-        this.setState({ errorFlag: true });
-        return
-      }
-    }
-    const newItem = this.state.user;
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      user: { name: '', surname: '', gender: '' }
-    }));
-  }
-
+  
   render() {
-    const user = this.state.user;
-    const items = this.state.items;
 
     return (
       <Router>
@@ -71,19 +37,16 @@ class App extends React.Component {
             <Route path="/home">
               <Home />
             </Route>
+            <Route path="/user-details">
+              <UserDetails />
+            </Route>
+
           </Switch>
         </div>
       </Router>
     );
   }
-  // return (
-  //   <div className="App">
-  //     <Userform user={user} items={items} onUserChange={this.handleChange} onUserSubmit={this.handleSubmit} />
-  //     {(this.state.errorFlag) && (<ErrorMsg text='Fill all fields first' />)}
-  //     {(this.state.items.length > 0) &&
-  //       (<Output items={this.state.items} />)}
-  //   </div>
-  // );
+  
 }
 
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import ErrorMsg from './ErrorMsg'
+import Userform from './Userform'
+import Output from './Output'
+
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            user: {
+                name: '',
+                surname: '',
+                gender: ''
+            },
+            errorFlag: false
+        }
+    };
+
+    handleChange = (target, value) => {
+        this.setState({ user: { ...this.state.user, [target]: value }, errorFlag: false })
+    }
+
+    handleSubmit = () => {
+        for (let key in this.state.user) {
+            if (this.state.user[key] === '') {
+                this.setState({ errorFlag: true });
+                return
+            }
+        }
+        const newItem = this.state.user;
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            user: { name: '', surname: '', gender: '' }
+        }));
+    }
+
     render() {
+        const user = this.state.user;
+        const items = this.state.items;
+
+
         return (
-            <h2>Home Page</h2>
-        )
+            <div className="App">
+                <h2>Home Page</h2>
+                <Userform user={user} items={items} onUserChange={this.handleChange} onUserSubmit={this.handleSubmit} />
+                {(this.state.errorFlag) && (<ErrorMsg text='Fill all fields first' />)}
+                {(this.state.items.length > 0) &&
+                    (<Output items={this.state.items} />)}
+            </div>
+        );
     }
 }
 
